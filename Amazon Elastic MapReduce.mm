b825bb35d14b0b459081ec11c10cc2a6bb854da0
martinliu <martin@aws-faq.com>
2016-03-10 17:27:26 +0800

    modified theme

--- Amazon Elastic MapReduce.mm
+++ Amazon Elastic MapReduce.mm
@@ -1,4 +1,97 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1457600761829" ID="ID_713982238" MODIFIED="1457600792229" TEXT="Amazon Elastic MapReduce"/>
+<node COLOR="#000000" CREATED="1457600761829" ID="ID_713982238" MODIFIED="1457601937398" TEXT="Amazon Elastic MapReduce">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1457600962383" ID="ID_246328400" MODIFIED="1457601937397" POSITION="right" TEXT="Hadoop">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="idea"/>
+<node COLOR="#00b439" CREATED="1457600977765" ID="ID_1328213418" MODIFIED="1457601568526" TEXT="open source project">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1457600990211" ID="ID_246098741" MODIFIED="1457601568528" TEXT="written in java">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1457601069664" ID="ID_1712845959" MODIFIED="1457601568529" TEXT="MapReduce is a distributed processing architecture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1457601011777" ID="ID_705045267" MODIFIED="1457601568531" TEXT="build for">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1457601016185" ID="ID_1311660886" MODIFIED="1457601937396" TEXT="handling uncertainty">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1457601024855" ID="ID_1979388002" MODIFIED="1457601937396" TEXT="handling data variety">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1457601033615" ID="ID_95278366" MODIFIED="1457601937396" TEXT="solution flexibility">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1457601041559" ID="ID_1789391365" MODIFIED="1457601937396" TEXT="handling data volume, velocity">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1457601188109" ID="ID_247038052" MODIFIED="1457601568535" TEXT="benefits">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1457601197781" ID="ID_1884171688" MODIFIED="1457601937396" TEXT="handling uncertainty">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1457601206922" ID="ID_146511621" MODIFIED="1457601937395" TEXT="handling data variety">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1457601215741" ID="ID_1154450227" MODIFIED="1457601937395" TEXT="providing flexibility">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1457601229409" ID="ID_95682389" MODIFIED="1457601937395" TEXT="handling data volume and velocity">
+<edge STYLE="bezier"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1457601261154" ID="ID_900563309" MODIFIED="1457601937394" POSITION="right" TEXT="advantages">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="button_ok"/>
+<node COLOR="#00b439" CREATED="1457601432594" ID="ID_1437400350" MODIFIED="1457601568546" TEXT="cluster setup and resizing takes minutes">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1457601458022" ID="ID_1904988404" MODIFIED="1457601568548" TEXT="easily create application-specific cluster configurations">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1457601481823" ID="ID_1851220885" MODIFIED="1457601568555" TEXT="container managed service makes software installation and upgrades trivial">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1457601522395" ID="ID_1045997749" MODIFIED="1457601990525">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      cost is usually much lower
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
 </map>
