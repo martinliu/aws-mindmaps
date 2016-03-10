ba6c86a0590a6f18f07515fddf124c65953ff694
martinliu <martin@aws-faq.com>
2016-03-10 17:07:18 +0800

    Amazon EMR

---
+++ Amazon Elastic MapReduce.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1457600761829" ID="ID_713982238" MODIFIED="1457600792229" TEXT="Amazon Elastic MapReduce"/>
+</map>
